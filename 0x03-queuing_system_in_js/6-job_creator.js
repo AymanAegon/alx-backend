ec34836ac618b449167172c0a88ac1c4040cba43
Ayman Ammar <ayman24ammar@gmail.com>
2024-03-05 16:15:31 +0100

    fix

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,9 +2,12 @@ import { createQueue } from 'kue';
 
 const queue = createQueue();
 const job = queue.create('push_notification_code', {
-  phoneNumber: 'string',
+  phoneNumber: '0612345678',
   message: 'string',
 });
+
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', () => console.log('Notification job failed'));
 
 job.save((err) => {
   if (!err) console.log('Notification job created:', job.id);
