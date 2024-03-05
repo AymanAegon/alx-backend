ef0044de91c3619321039e0d3f4fe700c4f62b96
Ayman Ammar <ayman24ammar@gmail.com>
2024-03-05 17:40:50 +0100

    10

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,20 @@
+function createPushNotificationsJobs(jobs, queue) {
+  if (!(jobs instanceof Array)) {
+    throw Error('Jobs is not an array');
+  }
+  jobs.forEach((obj) => {
+    const job = queue.create('push_notification_code_3', obj);
+
+    job.on('complete', () => console.log(`Notification job #${job.id} completed`));
+    job.on('failed', (err) => console.log(`Notification job #${job.id} failed: ${err}`));
+    job.on('progress', (progress) => {
+      console.log(`Notification job #${job.id} ${progress}% complete`);
+    });
+
+    job.save((err) => {
+      if (!err) console.log('Notification job created:', job.id);
+    });
+  });
+}
+
+module.exports = createPushNotificationsJobs;
